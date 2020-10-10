5d6cf41edcb96a8fbc6b48d1ad71b7e2fc2789bf
김승민 <ksmani0@gmail.com>
2020-10-10 13:11:38 +0900

    save

---
+++ seungmki_week1/Hello World.cs
@@ -0,0 +1,14 @@
+using System;
+
+using System.Threading.Tasks;
+
+namespace OutPut_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
