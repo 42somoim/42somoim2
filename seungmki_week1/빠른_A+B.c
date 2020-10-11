38b92c9f6e1234ee3666fae6cbdb34b9ecc76605
김승민 <ksmani0@gmail.com>
2020-10-11 13:15:44 +0900

    week1

---
+++ seungmki_week1/빠른_A+B.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int i, a, b;
+    scanf("%d", &i);
+    while (i-- != 0) {
+        scanf("%d %d", &a, &b);
+        printf("%d\n", a + b);
+    }
+    return 0;
+}
