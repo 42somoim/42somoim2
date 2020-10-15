131b9c35f6723eede6aead5b8a5647f3fff284c4
ArimYeon <48411387+ArimYeon@users.noreply.github.com>
2020-10-16 00:20:01 +0900

    week2
    week2

---
+++ ayeon/week2/bj11727.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+//dp[n] = dp[n-1] + dp[n-2]*2
+
+int dp[1001];
+
+int main(){
+	int n;
+	scanf("%d", &n);
+	dp[1] = 1;
+	dp[2] = 3;
+	for (int i = 3; i<=n; i++){
+		dp[i] = (dp[i-1] + dp[i-2] * 2) % 10007;
+	}
+	printf("%d\n", dp[n]);
+	return 0;
+}
