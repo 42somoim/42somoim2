14a53e9e02f10a9e219c918993cfeb4133e8408a
hanjongho <lgwl81@gmail.com>
2020-10-17 17:47:06 +0900

    upload

---
+++ johan/2주차/1463.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int dp[1000001], number;
+
+    dp[1] = 0;
+    cin >> number;
+    for (int i = 2; i <= number; i++)
+    {
+        dp[i] = dp[i - 1] + 1;
+        if (i % 2 == 0 && dp[i] > dp[i / 2] + 1)
+            dp[i] = dp[i / 2] + 1;
+        if (i % 3 == 0 && dp[i] > dp[i / 3] + 1)
+            dp[i] = dp[i / 3] + 1;
+    }
+    cout << dp[number];
+}
