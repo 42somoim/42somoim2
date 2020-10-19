4bb6a072654abc5afe4e820836c1c944059a4078
김건우 <papawolf@kakao.com>
2020-10-19 19:59:03 +0900

    Take my fee... very sorry

--- gunkim/기찍_N.c
+++ gunkim/기찍_N.c
@@ -1,25 +1,26 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   11718.c                                            :+:      :+:    :+:   */
+/*   기찍_N.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: gunkim <papawolf@kakao.com>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/10/19 07:28:52 by gunkim            #+#    #+#             */
-/*   Updated: 2020/10/19 11:22:27 by gunkim           ###   ########.fr       */
+/*   Created: 2020/10/19 19:13:46 by gunkim            #+#    #+#             */
+/*   Updated: 2020/10/19 19:18:36 by gunkim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
-#include <stdlib.h>
 
 int		main(void)
 {
-	char	str[101];
+	unsigned int		num;
 
-	while (fgets(str, 101, stdin))
+	scanf("%d", &num);
+	while (num)
 	{
-		printf("%s", str);
+		printf("%d\n", num);
+		num--;
 	}
 	return (0);
 }
