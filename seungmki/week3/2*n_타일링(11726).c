f0a7b46f122c50684c3dd5d1db5d4bc6c33e72eb
김승민 <ksmani0@gmail.com>
2020-10-26 08:52:43 +0900

    week3_dp

---
+++ seungmki/week3/2*n_타일링(11726).c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int n, i;
+    int memo[1001];
+    scanf("%d", &n);
+
+    memo[1] = 1;
+    memo[2] = 2;
+    for (i = 3; i <= n; ++i) {
+        memo[i] = (memo[i - 1] + memo[i - 2]) % 10007;
+    }
+    printf("%d\n", memo[n]);
+
+    return 0;
+}
