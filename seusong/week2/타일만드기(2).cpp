2a2106961ad4efb41f403d3c83c53d1a94200675
Evan Song <qhftkf1@naver.com>
2020-10-25 11:31:26 +0900

    week2

--- seusong/week2/타일만드기(2).cpp
+++ seusong/week2/타일만드기(2).cpp
@@ -8,9 +8,9 @@
 //
 //	cin >> n;
 //	arr[1] = 1;
-//	arr[2] = 2;
+//	arr[2] = 3;
 //	for (int i = 3; i <= n; i++) {
-//		arr[i] = (arr[i - 1] + arr[i - 2]) % 10007;
+//		arr[i] = (2 * arr[i - 1] + arr[i - 2]) % 10007;
 //	}
 //	cout << arr[n] << "\n";
 //}
