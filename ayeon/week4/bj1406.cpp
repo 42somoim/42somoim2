9c899fb69484a1f27c5c67f3de3ea199f3cf495f
ArimYeon <48411387+ArimYeon@users.noreply.github.com>
2020-11-04 22:40:51 +0900

    week4
    week4

--- ayeon/week4/bj1406.cpp
+++ ayeon/week4/bj1406.cpp
@@ -1,9 +1,49 @@
 #include <iostream>
-#include <list>
+#include <stack>
 using namespace std;
 
 int main(){
-	ios::sync_with_stdio(false); cin.tie(NULL);
-
+	ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+	stack<char> left, right;
+	int n;
+	string str;
+	cin >> str;
+	cin >> n;
+	for (auto ch : str)
+		left.push(ch);
+	while (n--){
+		char c, s;
+		cin >> c;
+		if (c == 'P'){
+			cin >> s;
+			left.push(s);
+		}
+		else if (c == 'L'){
+			if (!left.empty()){
+				s = left.top();
+				left.pop();
+				right.push(s);
+			}
+		}
+		else if (c == 'D'){
+			if (!right.empty()){
+				s = right.top();
+				right.pop();
+				left.push(s);
+			}
+		}
+		else if (c == 'B'){
+			if (!left.empty())
+				left.pop();
+		}
+	}
+	while (!left.empty()){
+		right.push(left.top());
+		left.pop();
+	}
+	while (!right.empty()){
+		cout << right.top();
+		right.pop();
+	}
 	return 0;
 }
