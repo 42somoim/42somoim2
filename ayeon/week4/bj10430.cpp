295978686cd082acbae1f7247377a54c716aa9b2
ArimYeon <48411387+ArimYeon@users.noreply.github.com>
2020-11-08 19:58:34 +0900

    week4 finish(except bj1168)
    week4 finish(except bj1168)

---
+++ ayeon/week4/bj10430.cpp
@@ -0,0 +1,9 @@
+#include <cstdio>
+
+int main(){
+	int A, B, C;
+	scanf("%d %d %d", &A, &B, &C);
+	printf("%d\n%d\n", (A+B)%C, ((A%C)+(B%C))%C);
+	printf("%d\n%d\n", (A*B)%C, ((A%C)*(B%C))%C);
+	return 0;
+}
