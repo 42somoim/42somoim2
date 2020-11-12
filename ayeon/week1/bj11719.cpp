afbb7e40f288043e91a015d57c01cdc6c5a215a3
ji-park <qkrskdjf@naver.com>
2020-11-12 18:18:19 +0900

    test

--- ayeon/week1/bj11719.cpp
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main(){
-	char s[101];
-	while (fgets(s, 101, stdin)){
-		printf("%s", s);
-	}
-	return 0;
-}
--- mko/1주차/별찍기16.py
+++
@@ -1,7 +0,0 @@
-N = int(input())
-for i in range(N) :
-    for j in range(N-(i+1)) :
-        print(' ',end='')
-    for j in range(i+1) :
-        print('* ',end='')
-    print()
