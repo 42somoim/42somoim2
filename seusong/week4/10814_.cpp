895cf42a2cf67a11b27b4a23ad5ce0a66df61e29
seusong <seusong@c2r3s7.42seoul.kr>
2020-11-14 16:05:42 +0900

    20201114 10814

---
+++ seusong/week4/10814_.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<map>
+#include<vector>
+#include<set>
+#include<algorithm>
+
+using namespace std;
+
+bool compare(pair<int, string>a, pair<int, string>b) {
+	if (a.first == b.first) {
+		return false;
+	}
+	else {
+		return a.first < b.first;
+	}
+}
+int main(void){
+	int n;
+	pair<int, string> p;
+	vector<pair<int, string>> group;
+
+	cin >> n;
+	for(int i = 0; i < n; i++){
+		int age; string name;
+		cin >> age >> name;
+		p.first = age; p.second = name;
+		group.push_back(p);
+	}
+	sort(group.begin(), group.end(), compare);
+	for(pair<int, string> p1 : group)
+		cout << p1.first << " " << p1.second << "\n";
+}
