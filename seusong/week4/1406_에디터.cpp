2e324b478316083ae7838768d64601398f053936
song <qhftkf1@naver.com>
2020-11-16 16:16:34 +0900

    20201116 에디터

--- seusong/week4/1406_에디터.cpp
+++ seusong/week4/1406_에디터.cpp
@@ -5,8 +5,8 @@
 using namespace std;
 
 int main(void){
-	cin.tie(NULL);
-	ios::sync_with_stdio(false);
+	//cin.tie(NULL);
+	//ios::sync_with_stdio(false);
 	string s, ans;
 	char op, cha;
 	int n;
@@ -15,24 +15,20 @@ int main(void){
 	cin >> s >> n;
 	for(char c : s)
 		l.push_back(c);
-	l.push_back(' ');
 	auto iter = l.end();
 	for(int i = 0; i < n;i++){
 		cin >> op;
 		if(op == 'P'){
-			cin >> cha;
-			l.insert(iter++, cha);
+			cin >> cha; l.insert(iter, cha);
 		}
 		else if(op== 'L' && iter != l.begin())
 			iter--;
 		else if(op== 'D' && iter != l.end())
 			iter++;
 		else if(op== 'B' && iter != l.begin()){
-			l.erase(--iter);
+			iter--;
+			iter = l.erase(iter);
 		}
-		cout << *iter << "\n";
-		for(iter = l.begin(); iter != l.end();iter++)
-			cout << *iter;
 	}
 	for(iter = l.begin(); iter != l.end();iter++)
 		cout << *iter;
