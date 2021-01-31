be4efe0f6ad34b25095f2cbe81450e5b68a48e4c
song <qhftkf1@naver.com>
2021-01-31 22:26:17 +0900

    20210131 fail

--- seusong/week9/1744_수묶기.cpp
+++ seusong/week9/1744_수묶기.cpp
@@ -21,3 +21,54 @@ else if(a == 양수, b == 양수)
 
 
 */
+
+#include<iostream>
+#include<vector>
+#include<algorithm>
+
+using namespace std;
+
+int main(void){
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int n, num, zero, sum;
+	vector<int> minus;
+	vector<int> plus;
+
+	cin >> n;
+	zero = 0; sum = 0;
+	for(int i = 0; i < n ;i++){
+		cin >> num;
+		if(num > 0)
+			plus.push_back(num);
+		else if(num < 0)
+			minus.push_back(num);
+		else zero = 1;
+	}
+	sort(plus.begin(), plus.end());
+	sort(minus.begin(), minus.end());
+	// plus 처리
+	if(plus.size() % 2 == 1){
+		sum += plus[0];
+		for(int i = 1; i < plus.size(); i += 2){
+			sum += plus[i] * plus[i + 1];
+		}
+	}else{
+		for(int i = 0; i < plus.size(); i += 2){
+			sum += plus[i] * plus[i + 1];
+		}
+	}
+	//
+	if(minus.size() % 2 == 1){
+		for(int i = 0; i < minus.size() - 1; i += 2){
+			sum += minus[i] * minus[i + 1];
+		}
+		if(!zero) sum += minus[minus.size() - 1];
+	}else{
+		for(int i = 0; i < minus.size(); i += 2){
+			sum += minus[i] * minus[i + 1];
+		}
+	}
+	cout << sum;
+}
